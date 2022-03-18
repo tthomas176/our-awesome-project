8a44d6e5a6ce8d34627a5724b3fe3018bf57c641
JacksonCunnane <101876747+JacksonCunnane@users.noreply.github.com>
2022-03-18 10:04:32 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,19 +1,113 @@
-scene.setBackgroundColor(13)
-let mySprite = sprites.create(img`
+namespace SpriteKind {
+    export const goal = SpriteKind.create()
+}
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+	
+})
+scene.setBackgroundColor(7)
+let kicker = sprites.create(img`
     . . . . . . . . . . . . . . . . 
-    . . . . . . . . c c c c . . . . 
-    . . . . . . c c d d d d c . . . 
-    . . . . . c c c c c c d c . . . 
-    . . . . c c 4 4 4 4 d c c . . . 
-    . . . c 4 d 4 4 4 4 4 1 c . c c 
-    . . c 4 4 4 1 4 4 4 4 d 1 c 4 c 
-    . c 4 4 4 4 1 4 4 4 4 4 1 c 4 c 
-    f 4 4 4 4 4 1 4 4 4 4 4 1 4 4 f 
-    f 4 4 4 f 4 1 c c 4 4 4 1 f 4 f 
-    f 4 4 4 4 4 1 4 4 f 4 4 d f 4 f 
-    . f 4 4 4 4 1 c 4 f 4 d f f f f 
-    . . f f 4 d 4 4 f f 4 c f c . . 
-    . . . . f f 4 4 4 4 c d b c . . 
-    . . . . . . f f f f d d d c . . 
-    . . . . . . . . . . c c c . . . 
+    . . . . . . 2 2 2 2 2 e . . . . 
+    . . . . . 2 2 2 2 d 2 2 e . . . 
+    . . . . e 2 2 2 2 2 2 2 e . . . 
+    . . . . e 2 2 2 2 2 2 2 e . . . 
+    . . . . e 2 2 2 2 2 e f f c c . 
+    . . . . e e 2 2 e f f f f b c . 
+    . . . e e e f e 2 b f f f d c . 
+    . . e e 2 2 d f 2 1 1 1 1 b c . 
+    . . e e 2 2 d f e e c c c . . . 
+    . . b 1 1 d e 2 e e c . . . . . 
+    . . f f f f d d f . . . . . . . 
+    f f f f f f d d . . . . . . . . 
+    f f f . f f f e . . . . . . . . 
+    f f . . . . e e e . . . . . . . 
+    . . . . . . e e e e . . . . . . 
     `, SpriteKind.Player)
+let myFootball = darts.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . e e e e e e e e e e . . . 
+    . . e e e e e e e e e e e e . . 
+    . e e e e 1 e 1 e 1 e 1 e e e . 
+    . e e e 1 1 1 1 1 1 1 1 1 e e . 
+    . e e e e 1 e 1 e 1 e 1 e e e . 
+    . . e e e e e e e e e e e e . . 
+    . . . e e e e e e e e e e . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Projectile, 30, 75)
+myFootball.setTrace()
+myFootball.controlWithArrowKeys()
+let post = sprites.create(img`
+    .............cc.
+    ............cbbc
+    ............cbbc
+    ...........bdcc.
+    ...........bdbb.
+    ..........bddc..
+    ..........bdbb..
+    .........bddc...
+    .........bdbb...
+    ........bddc....
+    ........bdbb....
+    .......bddc.....
+    .......bdbb.....
+    ......bddc......
+    ......bdbb......
+    .....bddc.......
+    .....bdbb.......
+    ....bddc........
+    ....bdbb........
+    ...bddc.........
+    ...bdbb.........
+    ..bddc..........
+    ..bdbb..........
+    .bddc...........
+    .bdbb...........
+    b1dc............
+    b11c............
+    b11c............
+    b11c.........cc.
+    b11c........cbbc
+    b11c........cbbc
+    b11c.......bdcc.
+    b11c.......bdbb.
+    b11c......bddc..
+    b11bccc...bdbb..
+    b11bbbbcccddc...
+    b11bcccbccdbb...
+    b11b..ccbddc....
+    b11b...ccdbbc...
+    b11b...bddcbc...
+    b11b...bdbbcbc..
+    b11b..bddc.fbc..
+    b11b..bdbb.fbf..
+    b11b.bddc..fcf..
+    b11b.bdbb..fcf..
+    b11bbddc...fcf..
+    b11bbdbb...fcf..
+    b11bddc...cfcfc.
+    b11ddbb..cbfcfbc
+    b1dddc...cdfffdc
+    b1ddbb...cdcfcdc
+    cdddc....cbdddbc
+    cddbb....cbbbbbc
+    cddc.....cbbbbbc
+    cdbb.....cbbbbbc
+    .cc......cbbbbbc
+    .........cbbbbbc
+    .........cbbbbbc
+    .........cbbbbbc
+    .........8bbbbb8
+    .........8bbbbb8
+    .........6bbbbb6
+    ..........6bbb6.
+    ...........666..
+    `, SpriteKind.goal)
+post.setPosition(150, 50)
+kicker.setPosition(15, 50)
